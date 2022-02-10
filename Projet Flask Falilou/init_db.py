7e83c056e56eb2551288dce6c07ba61087e3513d
Falilou <90579427+falilou15@users.noreply.github.com>
2022-02-11 00:27:23 +0100

    Add files via upload

---
+++ Projet Flask Falilou/init_db.py
@@ -0,0 +1,16 @@
+import sqlite3
+
+connection = sqlite3.connect('database.db')
+
+
+with open('schema.sql') as f:
+    connection.executescript(f.read())
+
+cur = connection.cursor()
+
+cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
+            ('Premier Post', 'Contenu dans le premier post')
+            )
+
+connection.commit()
+connection.close()
